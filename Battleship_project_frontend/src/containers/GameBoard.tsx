f618b62ef0dd4694769783e608a0f81a9737c15e
JerosCalmera <136751073+JerosCalmera@users.noreply.github.com>
2023-10-11 16:19:31 +0100

    bugfix
    bugfix

--- Battleship_project_frontend/src/containers/GameBoard.tsx
+++ Battleship_project_frontend/src/containers/GameBoard.tsx
@@ -4,7 +4,7 @@ import { ReactComponentElement, useEffect, useState } from "react";
 import Grids from "../components/Grids";
 
 function GameBoard() {
-    const BASE_URL = "http://192.168.248.133"
+    const BASE_URL = "http://192.168.248.133."
 
     const [stompClient, setStompClient] = useState<Stomp.Client>(Stomp.over(new SockJS(`${BASE_URL}:8081/game`)));
     const [severStatus, setServerStatus] = useState(false)
@@ -14,10 +14,10 @@ function GameBoard() {
     const [enemyShipInfo, setEnemyShipInfo] = useState<string[]>([])
     const [enemyShipDamage, setEnemyShipDamage] = useState<string[]>([])
     const [shipDamage, setShipDamage] = useState<string[]>([])
-    const [roomReady, setRoomReady] = useState<boolean>(false)
-    const [players, setPlayers] = useState<number>(0)
     const [password, setPassword] = useState<string>("Pears")
     const [passwordEntry, setPasswordEntry] = useState<string>("Apples")
+    const [playerName, setPlayerName] = useState<string>("")
+    const [savedName, setSaveName] = useState<string>("")
 
     useEffect(() => {
         const port = 8081;
@@ -27,8 +27,7 @@ function GameBoard() {
         client.connect({}, () => {
             console.log("Connected to server");
             client.subscribe("/topic/connect", (message: any) => {
-                if (message.body.slice(12, -2) === "Wrong room number!") { console.log("Wrong room number!") }
-                else { serverSetMessageLog(message.body.slice(12, -2)) }
+                serverSetMessageLog(message.body.slice(12, -2))
             });
 
             client.subscribe("/topic/gameData", (message: any) => {
@@ -63,23 +62,34 @@ function GameBoard() {
         setPasswordEntry(password)
     }
 
+    const nameSave = () => {
+        setSaveName(playerName)
+    }
+
     return (
         <>
             {severStatus == true ? <h4>Connected to game server</h4> : <div><h4>Not connected to game server</h4> <button onClick={() => setAttemptReconnect(attemptReconnect + 1)}>Reconnect</button></div>}
             <h4>{serverMessageLog}</h4>
-            {serverMessageLog === "Room saved!" && passwordEntry === password ? <div>
-                < h1 > Room number: {passwordEntry}</h1 >
-                <h1>Waiting on other player.....</h1></div > :
-                serverMessageLog === "Rooms synced" ?
-                    <div>
-                        <Grids shipInfo={shipInfo} shipDamage={shipDamage} enemyShipInfo={enemyShipInfo} enemyShipDamage={enemyShipDamage} stompClient={stompClient} />
-                    </div>
-                    :
-                    <div>
-                        <h1>Please enter the room number....</h1>
-                        <input onChange={(e) => setPassword(e.target.value)}></input>
-                        <button onClick={auth}>Save</button>
-                    </div>
+            {savedName === "" ?
+                <div>
+                    <h1>Please enter your name....</h1>
+                    <input onChange={(e) => setPlayerName(e.target.value)}></input>
+                    <button onClick={nameSave}>Save</button>
+                </div>
+                :
+                serverMessageLog === "Room saved!" && playerName === savedName ? <div>
+                    < h1 > Room number: {passwordEntry}</h1 >
+                    <h1>Waiting on other player.....</h1></div > :
+                    serverMessageLog === "Rooms synced" ?
+                        <div>
+                            <Grids shipInfo={shipInfo} shipDamage={shipDamage} enemyShipInfo={enemyShipInfo} enemyShipDamage={enemyShipDamage} stompClient={stompClient} />
+                        </div>
+                        :
+                        <div>
+                            <h1>Please enter the room number....</h1>
+                            <input onChange={(e) => setPassword(e.target.value)}></input>
+                            <button onClick={auth}>Save</button>
+                        </div>
             }
         </>
     )
