71f521fe13302ae8ebd624def3c5ee6091263bd6
JerosCalmera <136751073+JerosCalmera@users.noreply.github.com>
2023-10-19 13:45:57 +0100

    kk

--- Battleship_project_frontend/src/containers/GameBoard.tsx
+++ Battleship_project_frontend/src/containers/GameBoard.tsx
@@ -6,7 +6,7 @@ import StartUp from "../components/StartUp";
 
 
 function GameBoard() {
-    const BASE_URL = "http://192.168.145.133"
+    const BASE_URL = "http://192.168.187.133"
 
     const [stompClient, setStompClient] = useState<Stomp.Client>(Stomp.over(new SockJS(`${BASE_URL}:8081/game`)));
     const [serverStatus, setServerStatus] = useState(false)
